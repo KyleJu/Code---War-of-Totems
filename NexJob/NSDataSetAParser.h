f5173f1d489dfdcc9cceaf42c1f21d68cfa498d8
Ann Kim <eunjkim27@gmail.com>
2015-02-21 14:56:05 -0800

    Added NSManagedObject subclass for NSDataSetA

---
+++ NexJob/NSDataSetAParser.h
@@ -0,0 +1,13 @@
+//
+//  NSDataSetAParser.h
+//  NexJob
+//
+//  Created by Ann Kim on 2015-02-21.
+//  Copyright (c) 2015 War of the Totems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDataSetAParser : NSObject
+
+@end
---
+++ NexJob/NSDataSetAParser.m
@@ -0,0 +1,13 @@
+//
+//  NSDataSetAParser.m
+//  NexJob
+//
+//  Created by Ann Kim on 2015-02-21.
+//  Copyright (c) 2015 War of the Totems. All rights reserved.
+//
+
+#import "NSDataSetAParser.h"
+
+@implementation NSDataSetAParser
+
+@end
