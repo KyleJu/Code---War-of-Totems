333c466bb1e48eb6406043865ff72e24ff247b60
Kyle Ju <xifengju@kyleju.local>
2015-02-21 17:22:36 -0800

    update

--- NexJob/NJJobVacancyParser.m
+++ NexJob/NJJobVacancyParser.m
@@ -7,6 +7,7 @@
 //
 
 #import "NJJobVacancyParser.h"
+#import "AppDelegate.h"
 
 @implementation NJJobVacancyParser
 
@@ -21,11 +22,14 @@
 
 +(void)traverseXMLElement:(TBXMLElement *)element {
     TBXMLElement *seriesElement = [TBXML childElementNamed:@"cansim:Series" parentElement:element];
+    id delegateContext = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [delegateContext managedObjectContext];
+    
     do{
         if ([[TBXML elementName:seriesElement] isEqualToString:@"cansim:Series"]){
             NSString *geoLocation = [TBXML valueOfAttributeNamed:@"GEO" forElement:seriesElement];
-            NSString *industry = [TBXML valueOfAttributeNamed:@"STATS" forElement:seriesElement];
-            NSString *jobType = [TBXML valueOfAttributeNamed:@"NAICS" forElement:seriesElement];
+            NSString *statesType = [TBXML valueOfAttributeNamed:@"STATS" forElement:seriesElement];
+            NSString *industry = [TBXML valueOfAttributeNamed:@"NAICS" forElement:seriesElement];
             
     
             TBXMLElement *eachElement = [TBXML childElementNamed:@"cansim:Obs" parentElement:seriesElement];
@@ -33,16 +37,48 @@
                 //create object here
                 NSString *statesYear = [TBXML valueOfAttributeNamed:@"TIME_PERIOD" forElement:eachElement];
                 NSString *vacancyValue = [TBXML valueOfAttributeNamed:@"OBS_VALUE" forElement:eachElement];
+                if (!([vacancyValue isEqualToString:@"F"] || [vacancyValue isEqualToString:@"x"])){
+                 JobVacancy *newVacancy = [NSEntityDescription insertNewObjectForEntityForName: @"JobVacancy" inManagedObjectContext:context];
+                    
+                    newVacancy.geoLocation = geoLocation;
+                    newVacancy.industry = industry;
+                    newVacancy.statsType = [statesType intValue];
+                    newVacancy.statesYear = [statesYear intValue];
+                    NSLog(@"The vacancy is %@ %@ %@ %@ %@", geoLocation, industry, statesType, statesYear, vacancyValue);
+                    switch ([statesType intValue] - 1) {
+                        case 0:{
+                            newVacancy.numberOfVacancy = [vacancyValue intValue];
+                            newVacancy.labourDemand = 0.0f;
+                            newVacancy.vacancyRate = 0.0f;
+                            break;
+                        }
+                        case 1:{
+                            newVacancy.numberOfVacancy = 0.0f;
+                            newVacancy.labourDemand = [vacancyValue intValue];
+                            newVacancy.vacancyRate = 0.0f;
+                            break;
+                        }
+                        case 2:{
+                            newVacancy.numberOfVacancy = 0.0f;
+                            newVacancy.labourDemand = 0.0f;
+                            newVacancy.vacancyRate = [vacancyValue intValue];
+                            break;
+                        }
+                            
+                        default:
+                            break;
+                    }
+                }
 
 
             }while ((eachElement = eachElement->nextSibling));
-            
-            
-            
         }
 
     }while ((seriesElement = seriesElement->nextSibling));
 
-
+    NSError *error = nil;
+    if (! [context save: &error]){
+        NSLog(@"%@", error);
+    }
 }
 @end
